23f8bcdc10a63e5256d9418a3d71f9d2293f109e
quanh21 <le.quocanhh21@gmail.com>
2022-05-13 14:49:48 +0700

    reflection

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -5,127 +5,203 @@ namespace Reflection
 {
     public class Program
     {
+        //public static void Copy(Customer customer,Employee employee)
+        //{
+        //    var parentProperties = customer.GetType().GetProperties();
+        //    var childProperties = employee.GetType().GetProperties();
+
+        //    foreach (var parentProperty in parentProperties)
+        //    {
+        //        foreach (var childProperty in childProperties)
+        //        {
+        //            if (parentProperty.Name == childProperty.Name && parentProperty.PropertyType== childProperty.PropertyType)
+        //            {
+        //                childProperty.SetValue(employee, parentProperty.GetValue(customer));
+        //                break;
+        //            }
+        //        }
+        //    }
+
+        //}
         private static void Main()
         {
 
-          
 
 
-            //initializing the mapper
-            var config = new MapperConfiguration(cfg => 
-                cfg.CreateMap<Customer, Employee>()
-            );
+
+            Customer customer = new Customer()
+            {
+                Id = 1,
+                prop1 = "prop test 1",
+                prop2 = "prop test 2",
+                prop3 = "prop test 3",
+                prop4 = "prop test 4",
+                prop5 = "prop test 5",
+                prop6 = "prop test 6",
+                prop7 = "prop test 7",
+                prop8 = "prop test 8",
+                prop9 = "prop test 9",
+                prop10 = "prop test 10",
+                prop11 = "prop test 11",
+                prop12 = "prop test 12",
+                prop13 = "prop test 13",
+                prop14 = "prop test 14",
+                prop15 = "prop test 15",
+            };
+            Employee employee = new Employee();
+
 
             Type T = Type.GetType("Reflection.Customer");
+            Type TDes = Type.GetType("Reflection.Employee");
 
-
-
-            Console.WriteLine("Properties in Customer Class");
-            PropertyInfo[] properties = T.GetProperties();
-            foreach (PropertyInfo property in properties)
+            Console.WriteLine("Properties in Employe Class");
+            PropertyInfo[] Tproperties = T.GetProperties();
+            Console.WriteLine("Properties in Employe Class");
+            PropertyInfo[] TDesproperties = TDes.GetProperties();
+            foreach (PropertyInfo property in Tproperties)
             {
 
                 Console.WriteLine(property.PropertyType.Name + " " + property.Name);
             }
-            Console.WriteLine();
-
-            // Create object
-            Customer cus = new Customer()
+            Console.WriteLine("------------------");
+            foreach (PropertyInfo property in TDesproperties)
             {
-                CId = 1,
-                Cprop1 = "prop test 1",
-                Cprop2 = "prop test 2",
-                Cprop3 = "prop test 3",
-                Cprop4 = "prop test 4",
-                Cprop5 = "prop test 5",
-                Cprop6 = "prop test 6",
-                Cprop7 = "prop test 7",
-                Cprop8 = "prop test 8",
-                Cprop9 = "prop test 9",
-                Cprop10 = "prop test 10",
-                Cprop11 = "prop test 11",
-                Cprop12 = "prop test 12",
-                Cprop13 = "prop test 13",
-                Cprop14 = "prop test 14",
-                Cprop15 = "prop test 15",
-            };
 
-            ////Using automapper
-            var mapper = new Mapper(config);
-            var employee = mapper.Map<Employee>(cus);
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
+            }
+            Console.WriteLine("---------------");
+            foreach (var customerProperty in Tproperties)
+            {
+                foreach (var employeeProperty in TDesproperties)
+                {
+                    if (customerProperty.PropertyType.Name == employeeProperty.PropertyType.Name)
+                    {
+                        employeeProperty.SetValue(employee, customerProperty.GetValue(customer,null));
+                        break;
+                    }
+                }
+            }
+            foreach (var property in TDesproperties)
+            {
 
-            Console.WriteLine(employee.Eprop1);
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name + " " + property.GetValue(customer));
+            }
+
+         
+          
 
 
-            //Console.WriteLine(emp);
+
+            ////Type T = Type.GetType("Reflection.Customer");
+            //Type T = typeof(Customer);
+
+
+            //Type TDes = typeof(Employee);
+            //PropertyInfo[] parentProperties = customer.GetType().GetProperties();
+            //PropertyInfo[] childProperties = employee.GetType().GetProperties();
+
+            ////Console.WriteLine("Properties in Customer Class");
+            ////PropertyInfo[] customerProperties = T.GetProperties();
+            ////Console.WriteLine("Properties in Employee Class");
+            ////PropertyInfo[] employeeProperties = TDes.GetProperties();
+            ////foreach (var property in parentProperties)
+            ////{
+
+            ////    Console.WriteLine(property.PropertyType.Name + " " + property.Name + " " + property.GetValue(customer));
+            ////}
+            //Console.WriteLine("------------");
+
+            //foreach (var property in childProperties)
+            //{
+
+            //    Console.WriteLine(property.PropertyType.Name + " " + property.Name + " " + property.GetValue(customer));
+            //}
+            //foreach (var customerProperty in customerProperties)
+            //{
+            //    foreach (var employeeProperty in employeeProperties)
+            //    {
+            //        if (customerProperty.PropertyType.Name == employeeProperty.PropertyType.Name)
+            //        {
+            //            employeeProperty.SetValue(employee, customerProperty.GetValue(customer));
+            //            break;
+            //        }
+            //    }
+            //}
+            //foreach (var property in employeeProperties)
+            //{
+
+            //    Console.WriteLine(property.PropertyType.Name + " " + property.Name + " " + property.GetValue(customer));
+            //}
+            ////Console.WriteLine();
+
         }
     }
     public class Customer
     {
-        public int CId { get; set; }
+        public int Id { get; set; }
 
-        public string Cprop1 { get; set; }
+        public string prop1 { get; set; }
 
-        public string Cprop2 { get; set; }
+        public string prop2 { get; set; }
 
-        public string Cprop3 { get; set; }
+        public string prop3 { get; set; }
 
-        public string Cprop4 { get; set; }
+        public string prop4 { get; set; }
 
-        public string Cprop5 { get; set; }
+        public string prop5 { get; set; }
 
-        public string Cprop6 { get; set; }
+        public string prop6 { get; set; }
 
-        public string Cprop7 { get; set; }
+        public string prop7 { get; set; }
 
-        public string Cprop8 { get; set; }
+        public string prop8 { get; set; }
 
-        public string Cprop9 { get; set; }
+        public string prop9 { get; set; }
 
-        public string Cprop10 { get; set; }
+        public string prop10 { get; set; }
 
-        public string Cprop11 { get; set; }
+        public string prop11 { get; set; }
 
-        public string Cprop12 { get; set; }
+        public string prop12 { get; set; }
 
-        public string Cprop13 { get; set; }
+        public string prop13 { get; set; }
 
-        public string Cprop14 { get; set; }
+        public string prop14 { get; set; }
 
-        public string Cprop15 { get; set; }
+        public string prop15 { get; set; }
     }
     public class Employee
     {
-        public int EId { get; set; }
-        public string Eprop1 { get; set; }
+        public int Id { get; set; }
+        public string prop1 { get; set; }
 
-        public string Eprop2 { get; set; }
+        public string prop2 { get; set; }
 
-        public string Eprop3 { get; set; }
+        public string prop3 { get; set; }
 
-        public string Eprop4 { get; set; }
+        public string prop4 { get; set; }
 
-        public string Eprop5 { get; set; }
+        public string prop5 { get; set; }
 
-        public string Eprop6 { get; set; }
+        public string prop6 { get; set; }
 
-        public string Eprop7 { get; set; }
+        public string prop7 { get; set; }
 
-        public string Eprop8 { get; set; }
+        public string prop8 { get; set; }
 
-        public string Eprop9 { get; set; }
+        public string prop9 { get; set; }
 
-        public string Eprop10 { get; set; }
+        public string prop10 { get; set; }
 
-        public string Eprop11 { get; set; }
+        public string prop11 { get; set; }
 
-        public string Eprop12 { get; set; }
+        public string prop12 { get; set; }
 
-        public string Eprop13 { get; set; }
+        public string prop13 { get; set; }
 
-        public string Eprop14 { get; set; }
+        public string prop14 { get; set; }
 
-        public string Eprop15 { get; set; }
+        public string prop15 { get; set; }
 
     }
 }
